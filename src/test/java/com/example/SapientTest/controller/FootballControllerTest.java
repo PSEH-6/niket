e23b5cc3d708866135e0bc5b49861818d268bb2f
indiahiring <indiahiring@WKWIN9357093.global.publicisgroupe.net>
2019-11-30 12:47:19 +0530

    Parameter passed

--- src/test/java/com/example/SapientTest/controller/FootballControllerTest.java
+++ src/test/java/com/example/SapientTest/controller/FootballControllerTest.java
@@ -6,28 +6,28 @@ import org.junit.jupiter.api.Test;
 
 public class FootballControllerTest {
 	FootballController footballController = new FootballController();
-
+String countryId =41+"";
 	@Test
 	public void testCountryId() throws Exception {
-		assertEquals(footballController.getLeagueDetails().contains("41"), true);
-		;
+		assertEquals(footballController.getLeagueDetails(countryId).contains("41"), true);
+
 	}
 
 	@Test
 	public void testCountryName() throws Exception {
-		assertEquals(footballController.getLeagueDetails().contains("England"), true);
-		;
+		assertEquals(footballController.getLeagueDetails(countryId).contains("England"), true);
+
 	}
 
 	@Test
 	public void testLeagueId() throws Exception {
-		assertEquals(footballController.getLeagueDetails().contains("149"), true);
-		;
+		assertEquals(footballController.getLeagueDetails(countryId).contains("149"), true);
+
 	}
 
 	@Test
 	public void testLeagueName() throws Exception {
-		assertEquals(footballController.getLeagueDetails().contains("Championship"), true);
-		;
+		assertEquals(footballController.getLeagueDetails(countryId).contains("Championship"), true);
+
 	}
 }
